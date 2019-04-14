f6e291fae2b1dfcbff7fcdc27159477b4d99faa3
yanpliu <yanpliu@thoughtworks.com>
2019-04-14 11:53:20 +0800

    first commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static junit.framework.TestCase.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+    @Test
+    public void should_be_true() {
+        assertTrue(true);
+    }
+}
